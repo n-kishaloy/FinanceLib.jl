4f2eb0df1fb9de7129685825f6d0d483d7c1b5a5
Kishaloy Neogi <nkishaloy@yahoo.com>
2022-04-10 01:40:16 +0100

    Version 0.1.0

--- src/Statements/mod.jl
+++ src/Statements/mod.jl
@@ -15,6 +15,116 @@ You may see the github repository at <https://github.com/n-kishaloy/FinanceLib.j
 """
 module Statements
 
+using Chain: @chain
+
+balanceSheetCalcMap = 
+  [ 
+    (:Inventories, 
+    [:RawMaterials, :WorkInProgress, :FinishedGoods],
+    Symbol[]
+    )
+  , (:CurrentAssets,
+    [:Cash, :CurrentReceivables, :CurrentLoans, :CurrentAdvances, :OtherCurrentAssets, :CurrentInvestments, :Inventories],
+    Symbol[]
+    )
+  , (:NetPlantPropertyEquipment,
+    [:PlantPropertyEquipment],
+    [:AccumulatedDepreciation]
+    )
+  , (:NetLeaseRentalAssets,
+    [:LeasingRentalAssets],
+    [:AccumulatedAmortizationLeaseRental]
+    )
+  , (:NetIntangibleAssets,
+    [:IntangibleAssets, :IntangibleAssetsDevelopment],
+    [:AccumulatedAmortization]
+    )
+  , (:LongTermAssets,
+    [:AccountReceivables, :LongTermLoanAssets, :LongTermAdvances, :LongTermInvestments, :OtherLongTermAssets, :NetPlantPropertyEquipment, :NetLeaseRentalAssets, :Goodwill, :CapitalWip, :OtherTangibleAssets, :NetIntangibleAssets],
+    Symbol[]
+    )
+  , (:Assets,
+    [:CurrentAssets, :LongTermAssets],
+    Symbol[]
+    )
+  , (:CurrentLiabilities,
+    [:CurrentPayables, :CurrentBorrowings, :CurrentNotesPayable, :OtherCurrentLiabilities, :InterestPayable, :CurrentProvisions, :CurrentTaxPayables, :LiabilitiesSaleAssets, :CurrentLeasesLiability],
+    Symbol[]
+    )
+  , (:LongTermLiabilities,
+    [:AccountPayables, :LongTermBorrowings, :BondsPayable, :DeferredTaxLiabilities, :LongTermLeasesLiability, :DeferredCompensation, :DeferredRevenues, :CustomerDeposits, :OtherLongTermLiabilities, :PensionProvision, :TaxProvision, :LongTermProvision],
+    Symbol[]
+    )
+  , (:Liabilities,
+    [:CurrentLiabilities, :LongTermLiabilities],
+    Symbol[]
+    )
+  , (:Equity,
+    [:CommonStock, :PreferredStock, :PdInCapAbovePar, :PdInCapTreasuryStock, :RevaluationReserves, :Reserves, :RetainedEarnings, :AccumulatedOci, :MinorityInterests],
+    Symbol[] 
+    )
+
+  ] :: Vector{Tuple{Symbol, Vector{Symbol}, Vector{Symbol}}}
+
+profitLossCalcMap = 
+  [ (:Revenue,
+    [:OperatingRevenue, :NonOperatingRevenue],
+    [:ExciseStaxLevy]
+  )
+  , (:COGS,
+    [:CostMaterial, :DirectExpenses],
+    Symbol[]
+  )
+  , (:GrossProfit,
+    [:Revenue],
+    [:COGS]
+  )
+  , (:Pbitda,
+    [:GrossProfit, :OtherIncome],
+    [:Salaries, :AdministrativeExpenses, :ResearchNDevelopment, :OtherOverheads, :OtherOperativeExpenses, :OtherExpenses, :ExceptionalItems]
+  )
+  , (:Pbitx,
+    [:Pbitda],
+    [:Depreciation, :AssetImpairment, :LossDivestitures, :Amortization]
+  )
+  , (:Pbtx,
+    [:Pbitx, :InterestRevenue, :OtherFinancialRevenue],
+    [:InterestExpense, :CostDebt]
+  )
+  , (:Pbt,
+    [:Pbtx],
+    [:ExtraordinaryItems, :PriorYears]
+  )
+  , (:Pat,
+    [:Pbt],
+    [:TaxesCurrent, :TaxesDeferred]
+  )
+  , (:OtherComprehensiveIncome,
+    [:GainsLossesForex, :GainsLossesActurial, :GainsLossesSales, :FvChangeAvlSale],
+    [:OtherDeferredTaxes]
+  )
+  , (:TotalComprehensiveIncome,
+    [:Pat, :OtherComprehensiveIncome],
+    Symbol[]
+  )
+  ] :: Vector{Tuple{Symbol, Vector{Symbol}, Vector{Symbol}}}
+
+# cashFlowCalcMap = 
+
+
+
+function createCalcMaps(bM)
+  bCl = [x for (x, _, _) ∈ bM] :: Vector{Symbol}
+  bE = mapreduce( ((x,y,z),) -> (y...,z...), 
+      (z,y) -> [z...,filter(x -> x ∉ bCl , y)...] :: Vector{Symbol}, bM , init = Symbol[]  ) 
+  bCl, bE
+end
+
+balanceSheetCalcItems, balanceSheetEntries = createCalcMaps(balanceSheetCalcMap)
+profitLossCalcItems, profitLossEntries = createCalcMaps(profitLossCalcMap)
+
+
+
 
 
 end
--- src/Statements/perf.jl
+++ src/Statements/perf.jl
@@ -3,7 +3,11 @@ println("\n\n\nPerformance of FinanceLib.Statements functions")
 import FinanceLib.Statements as St
 
 
+@code_warntype St.createCalcMaps(St.balanceSheetCalcMap)
+@code_warntype St.createCalcMaps(St.profitLossCalcMap)
 
+println(St.profitLossCalcItems)
+println(St.profitLossEntries)
 
 
 
